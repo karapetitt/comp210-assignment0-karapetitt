dd7168ca4f2842e6ceeaae4fdd61fd3369739789
Kara Petitt <karapetitt2002@gmail.com>
2022-01-24 13:19:57 -0500

    creted sayHello function and printed it in main

--- src/a0/Main.java
+++ src/a0/Main.java
@@ -6,11 +6,13 @@ public class Main {
         // Make sure it is 10.0 or higher
         System.out.println("Your Java version: " + System.getProperty(("java.version")));
 
-        // Task 2 here
+        System.out.println(sayHello());
 
     }
 
-    // Task 1 here
+    public static String sayHello(){
+        return("Hello World");
+    }
 
 
 
